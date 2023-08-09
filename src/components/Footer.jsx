4698b0e2d25257c03a9ba783044ae7efd5c44e4d
shibu-sd <sd.shibu007@gmail.com>
2023-08-10 01:31:04 +0530

    initial-commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <footer>
+            <p>Copyright © shibu10</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+    return (
+        <div className="note">
+            <h1>Testing</h1>
+            <p>Test 1</p>
+        </div>
+    );
+}
+
+export default Note;
